af16cbc1ba000261e2fac6333d94b7cae19a6707
pangnem <sprnd645@gmail.com>
2021-11-29 13:29:30 +0900

    First Commit
    start.spring.io

---
+++ src/test/java/or/kr/aihub/mailsender/MailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package or.kr.aihub.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
