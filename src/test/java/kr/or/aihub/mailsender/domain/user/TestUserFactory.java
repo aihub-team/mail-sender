76926f4863efbf26420533153b9091a936081fb8
pangnem <sprnd645@gmail.com>
2022-01-07 16:46:54 +0900

    docs: TestUserFactory Javadoc 추가

--- src/test/java/kr/or/aihub/mailsender/domain/user/TestUserFactory.java
+++ src/test/java/kr/or/aihub/mailsender/domain/user/TestUserFactory.java
@@ -3,7 +3,18 @@ package kr.or.aihub.mailsender.domain.user;
 import kr.or.aihub.mailsender.domain.user.domain.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * 테스트 유저 생성 담당.
+ */
 public class TestUserFactory {
+    /**
+     * 테스트 유저를 생성합니다.
+     *
+     * @param username        유저 이름
+     * @param password        유저 비밀번호
+     * @param passwordEncoder 비밀번호 암호화 구현체
+     * @return 생성된 테스트 유저
+     */
     public static User create(String username, String password, PasswordEncoder passwordEncoder) {
         return User.builder()
                 .username(username)
