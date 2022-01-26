f3c4952ae4c751c63a691b349a5695c29edd8c75
pangnem <sprnd645@gmail.com>
2022-01-26 13:39:35 +0900

    feat: MailUser 도메인 추가

---
+++ src/main/java/kr/or/aihub/mailsender/domain/mail/transactional/domain/MailUser.java
@@ -0,0 +1,33 @@
+package kr.or.aihub.mailsender.domain.mail.transactional.domain;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MailUser {
+
+    @CsvBindByName
+    private String data;
+
+    @CsvBindByName
+    private String name;
+
+    @CsvBindByName
+    private String belong;
+
+    @CsvBindByName
+    private String division;
+
+    @CsvBindByName
+    private String email;
+
+    @Builder
+    public MailUser(String data, String name, String belong, String division, String email) {
+        this.data = data;
+        this.name = name;
+        this.belong = belong;
+        this.division = division;
+        this.email = email;
+    }
+}
