8e6b1231f428d5ccb39da049868d656c3cd51a1b
pangnem <sprnd645@gmail.com>
2022-02-03 17:10:18 +0900

    refactor: 일회용 상수 제거

--- src/main/java/kr/or/aihub/mailsender/domain/mail/transactional/errors/NotSupportedFileExtensionException.java
+++ src/main/java/kr/or/aihub/mailsender/domain/mail/transactional/errors/NotSupportedFileExtensionException.java
@@ -1,9 +1,8 @@
 package kr.or.aihub.mailsender.domain.mail.transactional.errors;
 
 public class NotSupportedFileExtensionException extends RuntimeException {
-    private static final String PREFIX_MESSAGE = "지원되지 않는 확장자입니다";
 
     public NotSupportedFileExtensionException(String extension) {
-        super(String.format(PREFIX_MESSAGE + ": %s", extension));
+        super(String.format("지원되지 않는 확장자입니다" + ": %s", extension));
     }
 }
